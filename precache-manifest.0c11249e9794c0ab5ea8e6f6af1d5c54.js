33f2355b1e42c1147c5509bdb503eddd0717761f
tsharon-byte <sharonova_t@inbox.ru>
2020-04-04 14:21:26 +0300

    Updates

---
+++ precache-manifest.0c11249e9794c0ab5ea8e6f6af1d5c54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03e962e0cda0a2a2e5c7ec6a431c481f",
+    "url": "/tsharon-byte/markdown-previewer/index.html"
+  },
+  {
+    "revision": "04394dc590a6a3d6c347",
+    "url": "/tsharon-byte/markdown-previewer/static/css/main.a44b8210.chunk.css"
+  },
+  {
+    "revision": "65f4befe551eff3fddc5",
+    "url": "/tsharon-byte/markdown-previewer/static/js/2.74f23df9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tsharon-byte/markdown-previewer/static/js/2.74f23df9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04394dc590a6a3d6c347",
+    "url": "/tsharon-byte/markdown-previewer/static/js/main.f0d9d506.chunk.js"
+  },
+  {
+    "revision": "d6d5c0dd6dd250c10e16",
+    "url": "/tsharon-byte/markdown-previewer/static/js/runtime-main.c588b7de.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/tsharon-byte/markdown-previewer/precache-manifest.0c11249e9794c0ab5ea8e6f6af1d5c54.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tsharon-byte/markdown-previewer/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
